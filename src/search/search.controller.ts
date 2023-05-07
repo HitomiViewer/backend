fe6123f31f7dad0ad836d1b76e72052c063ea97a
Anonymous <anonymous@example.com>
2023-05-07 21:15:56 +0900

    add page to search

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,5 +1,5 @@
 import { HitomiService } from '@app/hitomi';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, HttpException, Query } from '@nestjs/common';
 import { ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('search')
@@ -17,6 +17,16 @@ export class SearchController {
       example: 'language:korean tag:full_color',
     },
   })
+  @ApiQuery({
+    name: 'page',
+    type: Number,
+    required: false,
+    description: 'page number (entire data when not paged)',
+    schema: {
+      minimum: 1,
+      default: 1,
+    },
+  })
   @ApiOkResponse({
     status: 200,
     description: 'gallery ids',
@@ -28,8 +38,13 @@ export class SearchController {
       example: [2545347, 2545313, 2545300, 2545225, 2544654],
     },
   })
-  async getSearch(@Query('query') query: string) {
-    return await this.hitomi.getSearch(query);
+  async getSearch(@Query('query') query: string, @Query('page') page?: number) {
+    if (page && page < 1)
+      throw new HttpException('page must be greater than 0', 400);
+    if (!page) return await this.hitomi.getSearch(query);
+    return await this.hitomi
+      .getSearch(query)
+      .then((x) => x.slice((page - 1) * 25, page * 25));
   }
 
   @Get('/suggest')
