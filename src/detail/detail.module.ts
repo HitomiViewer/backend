da014bc42d22f4ba43c5eb4c9cf4feb3df6e264b
Anonymous <anonymous@example.com>
2023-07-06 07:26:45 +0900

    fix

--- src/detail/detail.module.ts
+++ src/detail/detail.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DetailController } from './detail.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { LogEntity } from 'src/log/log.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LogEntity])],
   controllers: [DetailController],
 })
 export class DetailModule {}
--- src/log/log.module.ts
+++ src/log/log.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LogController } from './log.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LogEntity } from './log.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LogEntity])],
   controllers: [LogController],
 })
 export class LogModule {}
