acd2183af433744bd409894054613dbea5d28d49
Anonymous <anonymous@example.com>
2023-10-15 16:22:01 +0900

    proxy test

--- libs/hitomi/src/hitomi.module.ts
+++ libs/hitomi/src/hitomi.module.ts
@@ -1,10 +1,16 @@
 import { Global, Module } from '@nestjs/common';
 import { HitomiService } from './hitomi.service';
 import { HttpModule } from '@nestjs/axios';
+import { SocksProxyAgent } from 'socks-proxy-agent';
 
 @Global()
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule.register({
+      httpAgent: new SocksProxyAgent('socks5h://warproxy:1080'),
+      httpsAgent: new SocksProxyAgent('socks5h://warproxy:1080'),
+    }),
+  ],
   providers: [HitomiService],
   exports: [HitomiService],
 })
